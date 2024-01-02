f1cf1aff2e0b8a669f5d2df153a3c18a4c72af10
tomknk <tomita.kanako.feb@gmail.com>
2024-01-02 22:45:35 +0900

    Lesson 18Chapter 8.6までコミット

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -20,6 +20,12 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
